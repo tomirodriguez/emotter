24c0952063acbea4fa2611c5b8466fb2f2b4a047
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2023-04-30 19:45:15 -0300

    Ignore build errors on vercel, swcMinidy

--- next.config.mjs
+++ next.config.mjs
@@ -21,5 +21,14 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
 };
 export default config;
