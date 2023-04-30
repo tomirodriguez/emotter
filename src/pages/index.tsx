bb62d4f2aecb8e636e0f6b114cc38d04eef383e6
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2023-04-30 12:06:10 -0300

    Increase emoji size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ const PostView = ({ post, author }: PostWithUser) => {
           <span>·</span>
           <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
